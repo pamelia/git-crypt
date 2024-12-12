af6a9e52c0a74fd8314b626ecf1975cc6db5f992
Marcus Pamelia <kore@mindwipe.org>
2024-12-12 17:34:46 +0100

    ✨ refactor: remove logging functionality from main.go

--- main.go
+++ main.go
@@ -1,27 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/pamelia/git-crypt/cmd"
-	"log"
-	"os"
 )
 
 func main() {
-	logFile, err := os.OpenFile("/tmp/git-crypt.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Printf("Failed to open log file: %v\n", err)
-		os.Exit(1)
-	}
-	defer func(logFile *os.File) {
-		err := logFile.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(logFile)
-
-	logger := log.New(logFile, "", log.LstdFlags) // Log with timestamp
-	logger.Printf("Command: %s, Arguments: %v\n", os.Args[0], os.Args[1:])
-
 	cmd.Execute()
 }

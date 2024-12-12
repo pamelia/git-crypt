064ec94ef295b14b9ccd5123f4a1229ec8787a2c
Marcus Pamelia <kore@mindwipe.org>
2024-12-12 17:18:12 +0100

    ✨ feat: add logging functionality

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/pamelia/git-crypt/cmd"
+import (
+	"fmt"
+	"github.com/pamelia/git-crypt/cmd"
+	"log"
+	"os"
+)
 
 func main() {
+	logFile, err := os.OpenFile("/tmp/git-crypt.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("Failed to open log file: %v\n", err)
+		os.Exit(1)
+	}
+	defer func(logFile *os.File) {
+		err := logFile.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(logFile)
+
+	logger := log.New(logFile, "", log.LstdFlags) // Log with timestamp
+	logger.Printf("Command: %s, Arguments: %v\n", os.Args[0], os.Args[1:])
+
 	cmd.Execute()
 }
